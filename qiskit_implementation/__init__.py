700418c6cf0ce16ffe42b172a3e6004e2e88d452
Shinpache Shimura <64576881+mncrftfrcnm@users.noreply.github.com>
2026-08-10 20:27:11 +0400

    Update __init__.py

--- qiskit_implementation/__init__.py
+++ qiskit_implementation/__init__.py
@@ -26,6 +26,6 @@ __all__ = [
     "verify_qiskit_profile",
 ]
 
-smal_tst_reversible_builduiamncasdasd = build_reversible_nand_circuit # to not make the ruff stop at the 
+smal_tst_reversibleuiamncasdasd = build_reversible_nand_circuit # to not make the ruff stop at the 
 # 'from .reversible import build_reversible_nand_circuit' import
 
