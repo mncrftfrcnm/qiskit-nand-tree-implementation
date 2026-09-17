7830b89861a561f38c6684cfe039a866c52f6e80
Shinpache Shimura <64576881+mncrftfrcnm@users.noreply.github.com>
2026-09-17 17:40:34 +0400

    Keep resource helper out of public API

--- qiskit_implementation/__init__.py
+++ qiskit_implementation/__init__.py
@@ -9,18 +9,15 @@ from .classifier import (
 from .evaluator import QuantumNandEvaluator
 from .oracles import build_bit_oracle
 from .query_walk import build_query_walk_circuit
-from .resources import CircuitResources, analyze_query_resources
 from .reversible import build_reversible_nand_circuit
 from .walk_parameters import NandExperimentConfig, WalkParameters, theoretical_parameters
 
 __all__ = [
-    "CircuitResources",
     "NandEvaluation",
     "NandExperimentConfig",
     "QiskitVerification",
     "QuantumNandEvaluator",
     "WalkParameters",
-    "analyze_query_resources",
     "build_bit_oracle",
     "build_query_walk_circuit",
     "build_reversible_nand_circuit",
