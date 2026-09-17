f2a86a627b070f0e051b5055620822b703f5a929
Shinpache Shimura <64576881+mncrftfrcnm@users.noreply.github.com>
2026-09-17 07:35:31 +0400

    Expose formula evaluation helpers

--- non_qiskit/__init__.py
+++ non_qiskit/__init__.py
@@ -6,6 +6,20 @@ from .calibration import CalibrationResult, calibrate_profile
 from .classical import ClassicalResult, evaluate_bottom_up, evaluate_short_circuit
 from .convergence import ConvergencePoint, product_formula_convergence
 from .exact_walk import WalkResult, run_continuous_walk
+from .formula import (
+    Formula,
+    Input,
+    Nand,
+    evaluate_formula,
+    format_formula,
+    formula_depth,
+    formula_size,
+    input_,
+    input_indices,
+    input_occurrences,
+    nand,
+)
+from .formula_walk import FormulaWalkGraph, FormulaWalkResult, build_formula_walk_graph, run_formula_walk
 from .graph import GraphMatrix, MatrixFormat, NandWalkGraph, build_walk_graph
 from .product_formula import SplitWalkResult, run_symmetric_split
 from .profiles import (
@@ -27,8 +41,13 @@ __all__ = [
     "CalibrationResult",
     "ClassicalResult",
     "ConvergencePoint",
+    "Formula",
+    "FormulaWalkGraph",
+    "FormulaWalkResult",
     "GraphMatrix",
+    "Input",
     "MatrixFormat",
+    "Nand",
     "NandTree",
     "NandWalkGraph",
     "ProfileVerification",
@@ -38,13 +57,23 @@ __all__ = [
     "SplitWalkResult",
     "WalkResult",
     "analyze_scattering",
+    "build_formula_walk_graph",
     "build_walk_graph",
     "calibrate_profile",
     "evaluate_bottom_up",
+    "evaluate_formula",
     "evaluate_short_circuit",
+    "format_formula",
+    "formula_depth",
+    "formula_size",
+    "input_",
+    "input_indices",
+    "input_occurrences",
+    "nand",
     "product_formula_convergence",
     "profile_for",
     "run_continuous_walk",
+    "run_formula_walk",
     "run_symmetric_split",
     "sampling_plan",
     "sparse_query_sampling_plan",
